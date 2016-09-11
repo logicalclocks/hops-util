dd16f5c2e7c3b4b4b3b8b52f086686bccc188d04
Jim Dowling <jdowling@sics.se>
2016-09-11 22:26:32 +0200

    Deploy script for maven repo and chef artifact publishing

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+git pull
+
+VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ./pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
+
+echo ""
+echo "Deploying version: $VERSION ... to maven repository"
+echo ""
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
+                      -DrepositoryId=sics-release-repository \
+                      -Dfile=./target/kafka-util-${VERSION}.jar \
+                      -DgroupId=io.hops \
+                      -DartifactId=kafka-util \
+                      -Dversion=${VERSION} \
+                      -Dpackaging=jar \
+                      -DpomFile=./pom.xml \
+-DgeneratePom.description="HopsWorks Kafka Utility"
+
+echo ""
+echo "Deploying kafa-util-${VERSION}.jar to snurran.sics.se"
+echo ""
+scp target/kafka-util-${VERSION}.jar glassfish@snurran.sics.se:/var/www/hops
