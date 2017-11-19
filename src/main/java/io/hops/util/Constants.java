35d5a88f1bb123b46b6573d9bfad68503aecf8d4
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2017-11-19 18:12:47 +0100

    HOPSWORKS-300 (#6)
    * Added waitJobs method
    
    * Finalize waitJobs method

---
+++ src/main/java/io/hops/util/Constants.java
@@ -0,0 +1,39 @@
+package io.hops.util;
+
+/**
+ *
+ * Constants used by HopsUtil.
+ */
+public class Constants {
+
+  public static final String HOPSWORKS_REST_APPSERVICE = "appservice";
+  public static final String PROJECTID_ENV_VAR = "hopsworks.projectid";
+  public static final String CRYPTO_MATERIAL_PASSWORD = "material_passwd";
+  public static final String K_CERTIFICATE_ENV_VAR = "k_certificate";
+  //System properties set by Hopsworks
+  public static final String KAFKA_FLINK_PARAMS = "kafka_params";
+  public static final String KEYSTORE_VAL_ENV_VAR = "keyPw";
+  public static final String JSON_JOBSTATE = "running";
+  public static final String HOPSWORKS_REST_RESOURCE = "hopsworks-api/api";
+  public static final String JSON_JOBIDS = "jobIds";
+  public static final String TRUSTSTORE_VAL_ENV_VAR = "trustPw";
+  public static final String HOPSWORKS_REST_CERTSERVICE = "certs";
+  public static final long WAIT_JOBS_INTERVAL = 5000;
+  public static final String JSON_KEYSTOREPWD = "keyStorePwd";
+  public static final String JOBTYPE_ENV_VAR = "hopsworks.job.type";
+  public static final String T_CERTIFICATE_ENV_VAR = "t_certificate";
+  public static final String HOPSWORKS_RESTENDPOINT = "hopsworks.restendpoint";
+  public static final String JOBNAME_ENV_VAR = "hopsworks.job.name";
+  public static final String APPID_ENV_VAR = "hopsworks.job.appid";
+  public static final String KEYSTORE_ENV_VAR = "hopsworks.keystore";
+  public static final String HOPSWORKS_PROJECTUSER_ENV_VAR = "hopsworks.projectuser";
+  public static final String PROJECTNAME_ENV_VAR = "hopsworks.projectname";
+  public static final boolean WAIT_JOBS_RUNNINGSTATUS = true;
+  public static final String KAFKA_TOPICS_ENV_VAR = "hopsworks.kafka.job.topics";
+  public static final String ELASTIC_ENDPOINT_ENV_VAR = "hopsworks.elastic.endpoint";
+  public static final String JSON_KEYSTORE = "keyStore";
+  public static final String KAFKA_CONSUMER_GROUPS = "hopsworks.kafka.consumergroups";
+  public static final String KAFKA_BROKERADDR_ENV_VAR = "hopsworks.kafka.brokeraddress";
+  public static final String TRUSTSTORE_ENV_VAR = "hopsworks.truststore";
+
+}
