b0c9d2239f704a7d7db8f9496b4e523b87ea3de8
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-01-23 18:04:14 +0100

    Add snapshot version and update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,20 @@ git pull
 mvn clean install
 
 VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ./pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
+REPOSITORY_TYPE='release'
+REPOSITORY_URL_TYPE=''
+
+if [[ ${VERSION} = *"SNAPSHOT"* ]]; then
+  REPOSITORY_TYPE="snapshot"
+  REPOSITORY_URL_TYPE='snapshot'
+fi
 
 echo ""
-echo "Deploying version: $VERSION ... to maven repository"
+echo "Deploying version: $VERSION ... to maven ${REPOSITORY_TYPE} repository"
 echo ""
-mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/repository \
-                      -DrepositoryId=sics-release-repository \
+
+mvn  deploy:deploy-file -Durl=scpexe://kompics.i.sics.se/home/maven/${REPOSITORY_URL_TYPE}repository \
+                      -DrepositoryId=sics-${REPOSITORY_TYPE}-repository \
                       -Dfile=./target/hops-util-${VERSION}.jar \
                       -DgroupId=io.hops \
                       -DartifactId=hops-util \
@@ -24,3 +32,16 @@ echo ""
 echo "Deploying hops-util-${VERSION}.jar to snurran.sics.se"
 echo ""
 scp target/hops-util-${VERSION}.jar glassfish@snurran.sics.se:/var/www/hops
+
+
+echo ""
+echo "Building javadoc"
+echo ""
+mvn generate-sources javadoc:javadoc
+
+echo ""
+echo "Deploying hops-util-${VERSION}.jar javadoc to snurran.sics.se"
+echo ""
+rsync -r --delete target/site/apidocs/ glassfish@snurran.sics.se:/var/www/hops/hops-util-javadoc/${VERSION}
+
+echo "Done!"
