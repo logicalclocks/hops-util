b211f81bf837f7cb77e9fafdf06a4b39e3e261f4
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-08-15 11:08:33 +0200

    remove git pull and maven clean install from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -e
-git pull
-
-mvn clean install
 
 VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ./pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
 REPOSITORY_TYPE='release'
